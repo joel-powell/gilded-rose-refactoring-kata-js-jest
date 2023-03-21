95e393efc2ed645f7e149b460a841642db0ef614
Joel Powell <joelhpowell@icloud.com>
2023-03-21 11:19:00 +0000

    passing second test for default case

--- src/shop.js
+++ src/shop.js
@@ -5,9 +5,12 @@ module.exports = class Shop {
 
   updateQuality() {
     this.items.forEach((item, index) => {
-      let { name, sellIn, quality } = item;
+      const { name } = item;
+      let { sellIn, quality } = item;
 
       quality -= 1;
+
+      if (quality < 0) quality = 0;
 
       sellIn -= 1;
 
